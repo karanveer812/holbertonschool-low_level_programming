23d0baf2c3d65c4c3b6d39dbfe57141f32845db5
Karanveer <karanveer812@gmail.com>
2023-10-14 23:07:11 -0700

    5. Hello, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void) {
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return 0;
+}
