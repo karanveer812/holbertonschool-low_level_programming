4f93b95d47abf2349241aadfb68954bd71baaf7b
Karanveer <karanveer812@gmail.com>
2023-10-14 23:09:16 -0700

    5. Hello, printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -5,7 +5,8 @@
  *
  * Return: 0 on success
  */
-int main(void) {
-	printf("with proper grammar, but the outcome is a piece of art,");
+int main(void) 
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return 0;
 }
