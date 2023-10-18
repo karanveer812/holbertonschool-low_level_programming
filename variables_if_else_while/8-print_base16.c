33b9941c9e7673b555bb9891353d7d5a22860976
Karanveer <karanveer812@gmail.com>
2023-10-17 22:49:11 -0700

    8. Hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - program starting point
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int num;
+	
+	num = 0;
+	while (num <= 15)
+	{
+		if (num < 10)
+                        putchar('0' + num);
+                else
+                        putchar('a' + num - 10);
+		num++;
+	}
+	
+	putchar(10);
+	return (0);
+}
