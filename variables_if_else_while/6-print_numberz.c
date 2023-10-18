ae19e7156b45b9384146b3d88355ea76152976fe
Karanveer <karanveer812@gmail.com>
2023-10-17 20:12:33 -0700

    6. Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - program starting point
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	
+	while (n <= 9)
+	{
+		putchar (48 + n);
+		n++;
+	}
+	putchar (10);
+
+	return (0);
+}
