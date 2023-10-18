490d1624775bcc628f2a1eba1e63f45b7780deb4
Karanveer <karanveer812@gmail.com>
2023-10-17 20:04:19 -0700

    4. When I was having that alphabet soup, I never thought that it would pay off

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - program starting point
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'q' && ch != 'e')
+			putchar (ch);
+		ch++;
+	}
+	putchar (10);
+
+	return (0);
+}
