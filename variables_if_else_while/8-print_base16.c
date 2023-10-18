5c996f11c2c8530bbf2859e6d503d6aa2a51769d
Karanveer <karanveer812@gmail.com>
2023-10-17 22:51:04 -0700

    8. Hexadecimal

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,14 +10,14 @@
 int main(void)
 {
 	int num;
-	
 	num = 0;
+
 	while (num <= 15)
 	{
 		if (num < 10)
-                        putchar('0' + num);
-                else
-                        putchar('a' + num - 10);
+			putchar('0' + num)
+		else
+			putchar('a' + num - 10);
 		num++;
 	}
 	
