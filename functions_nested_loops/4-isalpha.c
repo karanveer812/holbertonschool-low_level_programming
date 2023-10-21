b53dca8fe139c63ecaf3c23858bd12a9ce16ed45
Karanveer <karanveer812@gmail.com>
2023-10-21 04:31:44 -0700

    4. isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - check if the character is uppercase
+ * @c: Takes character as input
+ *
+ * Return: 1 if true
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
