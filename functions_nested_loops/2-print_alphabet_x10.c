ef8982c070391bb358d6c69507da3c39f7c6cad5
Karanveer <karanveer812@gmail.com>
2023-10-21 04:05:14 -0700

    2. 10 x alphabet"

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print Alphabet 10 times
+ */
+void print_alphabet_x10(void);
+{
+	int i = 0;
+	char ch = 'a';
+
+	while(i <= 10)
+	{
+		while(ch <= 'z')
+		{
+			_putchar(ch);
+		}
+	}
+	_putchar(ch);
+}
