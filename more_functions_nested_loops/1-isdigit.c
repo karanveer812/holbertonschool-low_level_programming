018a5cdd01302f9ba516de3eeb89538fc492d3d2
Karanveer <karanveer812@gmail.com>
2023-10-22 00:42:20 -0700

    0. isupper

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if character is uppercase
+ * @c: take character as input
+ *
+ * Return: 1 if true 0 if false
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
