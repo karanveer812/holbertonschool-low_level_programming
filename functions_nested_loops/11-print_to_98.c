227be5681ea8bd36336fd3c6beaacabb59d96ce3
Karanveer <karanveer812@gmail.com>
2023-10-21 20:01:46 -0700

    11. 98 Battery Street, the OG

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,14 +9,14 @@ void print_to_98(int n)
 {
 	while (n != 98)
 	{
-		if (abs(n) >= 100)
+		if (n >= 100 || n <= -100)
 		{
 			int x = (n / 10);
 			_putchar((x / 10) + '0');
 			_putchar((x % 10) + '0');
 			_putchar((n % 10) + '0');
 		}
-		else if (abs(n) > 10)
+		else if (n >= 10 n <= -10)
 		{
 			_putchar((n / 10) + '0');
 			_putchar((n % 10) + '0');
