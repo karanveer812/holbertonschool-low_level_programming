b2b574da3804a6fc1445481f7da3d9ce2f65af71
Karanveer <karanveer812@gmail.com>
2023-10-21 19:24:54 -0700

    10. a + b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - add two integers
+ * @a, @b: takes integers as an input
+ *
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return ((a + b));
+}
