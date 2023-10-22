71e8006630ed8c5cfbbeabff37afc54ed761acdb
Karanveer <karanveer812@gmail.com>
2023-10-22 01:21:37 -0700

    4. I believe in numbers and signs

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ */
+void more_numbers(void)
+{
+	int i = 0;
+	int y;
+
+	while (i <= 9)
+	{
+		y = 0;
+		while (y <= 14)
+		{
+			if (y < 10)
+			{
+				_putchar(y + '0');
+			}
+			else
+			{
+				 _putchar((y / 10) + '0');
+				 _putchar((y % 10) + '0');
+			}
+		}
+		i++;
+		_putchar('\n');
+	}
+}
