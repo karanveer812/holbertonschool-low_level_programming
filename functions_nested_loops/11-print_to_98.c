7996ce8f9bd8c6081ccbbfb9beb1be5829ea8b24
Karanveer <karanveer812@gmail.com>
2023-10-21 20:20:39 -0700

    11. 98 Battery Street, the OG

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -26,9 +26,12 @@ void print_to_98(int n)
 			_putchar((n / 10) + '0');
 			_putchar((n % 10) + '0');
 		}
-		_putchar(',');
-		_putchar(' ');
-		 if (n < 98)
+		if (n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		if (n < 98)
                 {
                         n++;
                 }
