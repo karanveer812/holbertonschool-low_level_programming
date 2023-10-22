b6b6980caad8838793f23284080c2860838cb9ff
Karanveer <karanveer812@gmail.com>
2023-10-22 00:35:57 -0700

    0. isupper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,8 @@
 /**
  * _isupper - check if character is uppercase
  * @c: take character as input
- * return: 1 if true 0 if false
+ * 
+ * Return: 1 if true 0 if false
  */
 int _isupper(int c)
 {
