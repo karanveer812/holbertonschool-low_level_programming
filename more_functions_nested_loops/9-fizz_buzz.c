86779dfe7a1aded36b2ca326414c69064c425fce
Karanveer <karanveer812@gmail.com>
2023-10-24 21:57:23 -0700

    9. Fizz-Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,9 @@ int main(void)
 	while (i <= 100)
 	{
 		if ((i % 3) == 0 && (i % 5) == 0)
+		{
 			printf("FizzBuzz");
+		}
 		else if ((i % 3) == 0)
 		{
 			printf("Fizz");
@@ -20,7 +22,8 @@ int main(void)
 		{
 			printf("Buzz");
 		}
-		else {
+		else 
+		{
 			printf("%d", i);
 		}
 		if (i < 100)
