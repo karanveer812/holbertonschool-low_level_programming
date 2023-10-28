4dfd494c3b1eebea7b87a4360b7e9bdc153ed8cf
Karanveer <karanveer812@gmail.com>
2023-10-27 19:56:04 -0700

    4. I can only go one way. I've not got a reverse gear

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _puts - output the string
- * @str: takes string as input
+ * print_rev - output the string in reverse
+ * @s: takes string as input
  */
 void print_rev(char *s)
 {
 	int len = 0;
 
-	while(s[len] != '\0')
+	while (s[len] != '\0')
 	{
 		len++;
 	}
