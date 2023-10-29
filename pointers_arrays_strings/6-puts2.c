328465c13331f6718c40317ddcfaf08c826856fa
Karanveer <karanveer812@gmail.com>
2023-10-28 20:28:13 -0700

    6. Half the lies they tell about me aren't true

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * rev_string - output the string in reverse
+ * @s: takes string as input
+ */
+void puts2(char *str)
+{
+	char x;
+
+	while (str[n] != '\0')
+	{
+		n++;
+		if (n % 2 == 0)
+		{
+			_putchar(str[n]);
+		}
+	}
+}
