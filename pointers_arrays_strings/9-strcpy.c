848f525014e23aba8f7974658df0b0a033590259
Karanveer <karanveer812@gmail.com>
2023-10-28 21:26:55 -0700

    9. strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - n number of integers from an array
+ * @a: takes int as input
+ * @n: takes int as input
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int n = 0;
+
+	while (src[n] != '\0')
+	{
+		dest[n] = src[n]
+		n++;
+	}
+	printf(dest);
+}
