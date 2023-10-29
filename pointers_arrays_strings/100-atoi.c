c79383713a89c747444e03c3ea33ae41d0e62e3f
Karanveer <karanveer812@gmail.com>
2023-10-28 21:49:07 -0700

    10. Great leaders are willing to sacrifice

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  _strcpy - copy string from source to destination
+ * @dest: destination variable
+ * @src: source string
+ *
+ * Return: copies string to the variable
+ */
+int _atoi(char *s);
+{
+	int n = 0;
+	int i = 0;
+
+	while (s[n] != '\0')
+	{
+		if ((s[n] >= '0' && s[n] <= '9') || s[n] == '+' || s[n] == '-')
+		{
+			s[i] = s[n];
+			i++;
+		}
+		n++;
+	}
+	puts(s);
+
+	return (0);
+}
