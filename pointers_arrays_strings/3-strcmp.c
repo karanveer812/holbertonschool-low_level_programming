a45d0a3590701b3382a54c4e6b4ba1cf438e5228
Karanveer <karanveer812@gmail.com>
2023-10-29 22:20:39 -0700

    3. strcmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - copy string from source to destination
+ * @dest: destination string
+ * @src: The input string
+ * @n: take integer as input
+ *
+ * Return: result string
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i]) 
+		{
+			return (s1[i] - s2[i]);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+
+	return (dest);
+}
