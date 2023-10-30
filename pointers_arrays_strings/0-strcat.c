5218affb6a04bad47c8961b96f629ce52cf692f5
Karanveer <karanveer812@gmail.com>
2023-10-29 17:34:25 -0700

    0. strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * puts_half - Print the second half of a string
+ * @str: The input string
+ */
+char *_strcat(char *dest, char *src)
+{
+	dest = dest + src + '\0';
+
+	return (dest);
+}
