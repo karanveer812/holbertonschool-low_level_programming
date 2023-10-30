50657801fe1aeb694ace7b35b61cbf9f88be7968
Karanveer <karanveer812@gmail.com>
2023-10-29 18:34:40 -0700

    0. strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -5,23 +5,25 @@
  * _strcat - Concatnate two strings
  * @dest: destination string
  * @src: The input string
+ *
+ * Return: result string
  */
 char *_strcat(char *dest, char *src)
 {
 	int n = 0;
 	int i = 0;
-	
+
 	while (dest[n] != '\0')
 	{
 		n++;
 	}
 
 	while (src[i] != '\0')
-        {
-               dest[n] = src[i];
-	       i++;
-	       n++;
-        }
+	{
+		dest[n] = src[i];
+		i++;
+		n++;
+	}
 
 	return (dest);
 }
