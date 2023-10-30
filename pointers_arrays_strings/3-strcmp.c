1c00d54832355fa9a129bee405b02d4df74345dd
Karanveer <karanveer812@gmail.com>
2023-10-29 22:27:17 -0700

    3. strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -13,15 +13,20 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while(str1[i] == str2[i])
 	{
-		if (s1[i] != s2[i]) 
+		if(str1[i] == '\0' || str2[i] == '\0')
 		{
-			return (s1[i] - s2[i]);
+			break;
 		}
-		else
-		{
-			return (0);
-		}
+		i++;
+	}
+	if(str1[i] == '\0' && str2[i] == '\0')
+	{
+		return 0;
+	}
+	else
+	{
+		return str1[i] - str2[i];
 	}
 }
