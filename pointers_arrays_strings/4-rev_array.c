4d11b715cab6c6380eda6eb2925de39b013a1066
Karanveer <karanveer812@gmail.com>
2023-10-31 17:43:05 -0700

    4. I am a kind of paranoid in reverse. I suspect people of plotting to make me happy

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - compare two strings
+ * @s1: First string
+ * @s2: second string
+ *
+ * Return: 0 if same
+ */
+void reverse_array(int *a, int n)
+{
+	int x = 0;
+	int y = n;
+
+	while (x <= y)
+	{
+		a[x] = a[y];
+	}
+}
