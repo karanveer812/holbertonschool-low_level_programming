ec9f1929ba9bef7eae5fa0c4407e28141f2dac51
Karanveer <karanveer812@gmail.com>
2023-10-31 20:10:04 -0700

    5. Always look up

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - Reverse a given array
+ * @a: Take array as an input
+ * @n: Number of elemaents in the arrray
+ */
+char *string_toupper(char *ch)
+{
+	if (ch >= 'a' && ch <= 'z')
+	{
+		ch = (ch -32);
+	}
+	return (ch)
+}
