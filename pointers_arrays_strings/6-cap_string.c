e126b1601749b722e3a6344ac2e45e685ef46257
Karanveer <karanveer812@gmail.com>
2023-10-31 20:42:45 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - Convert string to uppercase
+ * @str: Take string as an input
+ *
+ * Return: Converted string
+ */
+char *cap_string(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		if (str[len - 1] == ',' || str[len - 1] == ';' || str[len - 1] == '.' || str[len - 1] == '!' || str[len - 1] == '?' 
+		|| str[len - 1] == '"' || str[len - 1] == '(' || str[len - 1] == ')' || str[len - 1] =='{' || str[len - 1] == '}' || str[len - 1] == '0')
+		{
+			str[len] -= 32
+		}
+		len++;
+	}
+
+	return(str)
+}
