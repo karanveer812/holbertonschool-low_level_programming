82dd75585e9c912f907ac3f4971a7439db6cc350
Karanveer <karanveer812@gmail.com>
2023-11-01 20:13:00 -0700

    7. Mozart composed his music not for the elite, but for everybody

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - Convert string to uppercase
+ * @str: Take string as an input
+ *
+ * Return: Converted string
+ */
+char *string_toupper(char *str)
+{
+	int len = 0;
+	int i = 0;
+	char letters[5] = {a, e, o, t, l};
+	char numbers[5] = {4, 3, 0, 7, 1};
+
+	while (str[len] != '\0')
+	{
+		while (letters[i] <= 5)
+		{
+			if (str[len] == letters[i])
+			{
+				str[len] = numbers[i];
+			}
+		}
+		len++;
+	}
+	return (str);
+}
