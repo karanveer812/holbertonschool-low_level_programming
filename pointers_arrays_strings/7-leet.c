0cd18b9880582b60188fffc20159c74d2cf52e43
Karanveer <karanveer812@gmail.com>
2023-11-01 20:29:10 -0700

    7. Mozart composed his music not for the elite, but for everybody

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * string_toupper - Convert string to uppercase
+ * leet - Encodes the string in 1337
  * @str: Take string as an input
  *
  * Return: Converted string
@@ -12,19 +12,17 @@ char *leet(char *str)
 	int len = 0;
 	int i = 0;
 	char letters[5] = {'a', 'e', 'o', 't', 'l'};
-	char numbers[5] = {4, 3, 0, 7, 1};
+	char numbers[5] = {'4', '3', '0', '7', '1'};
 
 	while (str[len] != '\0')
 	{
 		while (i < 5)
 		{
-			putchar(str[len]);
-			putchar(letters[i]);
-			if (str[len] == letters[i])
+			if (*str[len] == letters[i])
 			{
 				str[len] = numbers[i];
-				i++;
 			}
+			i++;
 		}
 		len++;
 	}
