d8874d0343c9d3d81f609df6afde146c8a3e6cb9
Karanveer <karanveer812@gmail.com>
2023-11-01 17:35:15 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -22,7 +22,7 @@ char *cap_string(char *str)
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			if (str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '\n' || str[i - 1] == ' '
-                	|| str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] =='{' || str[i - 1] == '}' || i == 0)
+                	|| str[i - 1] == '"' || str[i - 1] == '\t' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] =='{' || str[i - 1] == '}' || i == 0)
                 	{
                         	str[i] = str[i] - 32;
                 	}
