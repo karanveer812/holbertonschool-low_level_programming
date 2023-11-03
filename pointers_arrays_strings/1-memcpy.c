55ca1bba1b7f2c2c415acad5ecd849cd215d23ff
Karanveer <karanveer812@gmail.com>
2023-11-03 04:45:16 -0700

    1. memcpy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - copy string from source to destination
+ * @dest: destination string
+ * @src: The input string
+ * @n: take integer as input
+ *
+ * Return: result string
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int c = 0;
+
+	while (src[c] != '\0' && c < n)
+	{
+		dest[c] = src[c];
+		c++;
+	}
+
+	while (n > c)
+	{
+		dest[c] = '\0';
+		c++;
+	}
+
+	return (dest);
+}
