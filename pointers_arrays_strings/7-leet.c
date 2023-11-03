dee02ae8f0c00cfe2ed4183740b7def3190ba73b
Karanveer <karanveer812@gmail.com>
2023-11-03 04:06:59 -0700

    7. Mozart composed his music not for the elite, but for everybody

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -10,15 +10,15 @@
 char *leet(char *str)
 {
 	int len = 0;
-	int i = 0;
 	char letters[5] = {'a', 'e', 'o', 't', 'l'};
 	char numbers[5] = {'4', '3', '0', '7', '1'};
 
 	while (str[len] != '\0')
 	{
+		int i = 0;
 		while (i < 5)
 		{
-			if (*str[len] == letters[i])
+			if (str[len] == letters[i])
 			{
 				str[len] = numbers[i];
 			}
