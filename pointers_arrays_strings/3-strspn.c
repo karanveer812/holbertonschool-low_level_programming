287c968a3ed731ffa524ba51aac2085190723fdf
Karanveer <karanveer812@gmail.com>
2023-11-04 01:22:38 -0700

    3. strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,17 +10,28 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0; 
+	int i = 0;
+	char contains = "n"
 	unsigned int byte = 0;
 
 	while (s[i] != '\0')
 	{
 		int n = 0;
 
-		while (accept[n] != '\0' && accept[n] == s[i])
+		while (accept[n] != '\0')
 		{
-			byte++;
+			contains = "n";
+
+			if (accept[n] == s[i])
+			{
+				byte++;
+				contains = "y";
+			}
 			n++;
+		}
+		if (contains == "n")
+		{
+			break;
 		}
 		i++;
 	}
