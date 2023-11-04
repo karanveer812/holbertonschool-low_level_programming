bffeae37c4401c4898e717e7d50deec31b7da06a
Karanveer <karanveer812@gmail.com>
2023-11-04 00:34:31 -0700

    3. strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -2,31 +2,24 @@
 #include <stddef.h>
 
 /**
- * _strchr - Find character in a string
+ * _strspn - find length of prefix
  * @s: Input string
- * @c: character to find
+ * @accept: comparision string
  *
- * Return: pointer to the character
+ * Return: number of bytes
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0;
+	int i = 0; _strspn
 	unsigned int byte = 0;
 
 	while (s[i] != '\0')
 	{
 		int n = 0;
 
-		while (accept[n] != '\0')
+		while (accept[n] != '\0' && accept[n] == s[i])
 		{
-			if (s[i] == accept[n])
-			{
-				byte++;
-			}
-			else
-			{
-				break;
-			}
+			byte++;
 			n++;
 		}
 		i++;
