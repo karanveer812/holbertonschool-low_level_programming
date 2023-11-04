2951633781ecdca0255764f155198c0f96dee133
Karanveer <karanveer812@gmail.com>
2023-11-03 23:23:55 -0700

    2. strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memset - set the memory byffer to the given byte
+ * @s: Input string
+ * @b: desired byte to set
+ * @n: number of characters to be replaced
+ *
+ * Return: desired string
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			char *p = s[i];
+			return (p);
+			break;
+		}
+	}
+	return(NULL);
+}
