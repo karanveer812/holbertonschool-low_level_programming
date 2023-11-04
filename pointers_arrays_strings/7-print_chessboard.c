17cc5509f44939c9cf14b1b643af2c158de886f7
Karanveer <karanveer812@gmail.com>
2023-11-04 02:37:13 -0700

    6. Chess is mental torture

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - set the memory byffer to the given byte
+ * @s: Input string
+ * @b: desired byte to set
+ * @n: number of characters to be replaced
+ *
+ * Return: desired string
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x = 0;
+
+	while (x <= (8*8))
+	{
+		_putchar(a[x]);
+		x++
+	}
+}
