da39cf3e18a6ea493267e4d9b23b054ed06ff4b8
Karanveer <karanveer812@gmail.com>
2023-11-03 23:29:05 -0700

    2. strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
  * _memset - set the memory byffer to the given byte
@@ -23,5 +22,5 @@ char *_strchr(char *s, char c)
 		}
 		i++;
 	}
-	return(NULL);
+	return("");
 }
