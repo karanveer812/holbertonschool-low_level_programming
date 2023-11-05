acca2a703d7472f445117c4f590deba725d05044
Karanveer <karanveer812@gmail.com>
2023-11-05 00:00:13 -0700

    2. Dreams feel real

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: takes string as input
+ */
+int _strlen_recursion(char *s)
+{
+	if (s[0] != '\0')
+	{
+		_strlen_recursion(s + 1);
+		return (0 + 1);
+	}
+}
